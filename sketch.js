7507d8842e479ce57804d04c6aa3deb3111e18fe
Vinícius Giles <viniciusgcp@gmail.com>
2017-03-02 17:48:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,51 +1,52 @@
-var bird, counter, best;
+var bird, counter;
 var pipes = [];
 
 function setup() {
-	createCanvas(400,600);
-	newGame(first=true);
+  createCanvas(400,600);
+  newGame(first=true);
 }
+
 function newGame(first=false) {
-	counter = new Counter();
-	bird = new Bird();
-	if(first) {
-		pipes = [new Pipe()]; 
-	} else {
-		pipes = [];
-	}
+  counter = new Counter();
+  bird = new Bird();
+  if(first) {
+    pipes = [new Pipe()]; 
+  } else {
+    pipes = [];
+  }
 }
+
 function draw() {
-	background("rgb(124, 215, 226)");
-	
-	for(var i = pipes.length-1; i >= 0; --i) {
-		pipes[i].show();
-		pipes[i].update();
+  background("rgb(124, 215, 226)");
 
-		if(pipes[i].hits(bird)) {
-			newGame();
-			break;
-			
-		}
-		if(!pipes[i].ultrapassado && pipes[i].notTouchedBy(bird)) {
-			pipes[i].ultrapassado = true;
-			counter.update();
-		}
-		if(pipes[i].offscreen()) {
-			pipes.splice(i, 1);
-		}
-	}
+  for(var i = pipes.length-1; i >= 0; --i) {
+    pipes[i].show();
+    pipes[i].update();
 
-	counter.show();	
-	bird.show();
-	bird.update();
+    if(pipes[i].hits(bird)) {
+      newGame();
+      break;
+    }
+    if(!pipes[i].ultrapassado && pipes[i].notTouchedBy(bird)) {
+      pipes[i].ultrapassado = true;
+      counter.update();
+    }
+    if(pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+  }
 
-	if(frameCount % 120 == 0) {
-		pipes.push(new Pipe());
-	}	
+  counter.show();	
+  bird.show();
+  bird.update();
 
+  if(frameCount % 120 == 0) {
+  pipes.push(new Pipe());
+  }	
 }
+
 function keyPressed() {
-	if(key==' ') {
-		bird.up();
-	}
+  if(key==' ') {
+    bird.up();
+  }
 }
